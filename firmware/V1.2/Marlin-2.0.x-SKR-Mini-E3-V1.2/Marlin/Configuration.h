40994cc5f750555a77e93ec41688af062907d522
Bartosz Mazurczak <bartosz.mazurczak@gigaset.com>
2019-10-29 06:52:04 +0100

    Author

--- firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
+++ firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(thisiskeithb, Ender-3)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Drops, Ender-3)" // Who made the changes.
 
 /**
  * *** VENDORS PLEASE READ ***
